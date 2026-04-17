270d28a9e6764369ed955e7563ac0436dfc393d3
liangwenxuan <845915012@qq.com>
2026-04-17 16:01:24 +0800

    commit

--- star-home/src/main/java/com/ruoyi/starhome/domain/dto/FurnitureVideoGenerationTaskPageItemResp.java
+++ star-home/src/main/java/com/ruoyi/starhome/domain/dto/FurnitureVideoGenerationTaskPageItemResp.java
@@ -1,5 +1,6 @@
 package com.ruoyi.starhome.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,8 @@ public class FurnitureVideoGenerationTaskPageItemResp {
     private String localFinalVideoUrl;
     private String remoteFinalVideoUrl;
     private String errorMessage;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 }
